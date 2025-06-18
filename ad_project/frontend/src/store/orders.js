ca7617e61e957a8f2d42728e0ee24063be62a1de
edem4ik322 <rustemov.edem@mail.ru>
2025-06-18 17:38:15 +0300

    финал

--- ad_project/frontend/src/store/orders.js
+++ ad_project/frontend/src/store/orders.js
@@ -1,14 +1,26 @@
+class Order {
+  constructor(name, phone, adId, userId, done = false, id = null) {
+    this.name = name
+    this.phone = phone
+    this.adId = adId
+    this.userId = userId
+    this.done = done
+    this.id = id
+  }
+}
+
 export default {
   state: {
     orders: []
   },
-  mutations: {},
+  mutations: {
+    createOrder(state, payload) {
+      state.orders.push(payload)
+    }
+  },
   actions: {
     async createOrder({commit}, {name, phone, adId, userId}) {
-      console.log(name)
-      console.log(phone)
-      console.log(adId)
-      console.log(userId)
+      let payload = new Order(name, phone, adId, userId, false, Math.random())
       commit('clearError', null, { root: true })
       // Заглушка запроса
       let isRequestOk = true
@@ -18,6 +30,7 @@ export default {
       if (isRequestOk) {
         await promise.then(()=> {
           // Здесь вызовем commit для добавления заказа
+          commit('createOrder', payload)
         })
       } else {
         await promise.then(()=> {
@@ -27,5 +40,10 @@ export default {
       }
     }
   },
-  getters: {}
+  getters: {
+    orders (state, getters, rootState, rootGetters) {
+      if (rootGetters.user == null) return []
+      return state.orders.filter(order => order.userId == rootGetters.user.id)
+    }
+  }
 } 
---
+++ ad_project/frontend/src/views/OrdersView.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>Orders</h1>
+    <v-list>
+      <v-list-item v-for="order in orders" :key="order.id">
+        <v-list-item-content>
+          <v-list-item-title>{{ order.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-checkbox v-model="order.done"></v-checkbox>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+  computed: {
+    orders() {
+      return this.$store.getters.orders
+    }
+  }
+}
+</script> 
