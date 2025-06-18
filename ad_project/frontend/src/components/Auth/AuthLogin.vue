3f3dbf200988651ce8009f2a5df9c10f0663f272
edem4ik322 <rustemov.edem@mail.ru>
2025-06-18 17:06:19 +0300

    14.4

--- ad_project/frontend/src/components/Auth/AuthLogin.vue
+++ ad_project/frontend/src/components/Auth/AuthLogin.vue
@@ -31,7 +31,8 @@
             <v-btn 
               color="primary"
               @click="onSubmit"
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
             >
               Login
             </v-btn>
@@ -58,6 +59,11 @@ export default {
         v => !!v || 'Password is required',
         v => (v && v.length >= 6) || 'Password must be more or equal than 6 characters'
       ]
+    }
+  },
+  computed: {
+    loading() {
+      return this.$store.getters.loading
     }
   },
   methods: {
