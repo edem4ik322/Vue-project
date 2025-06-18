0c8e80953d2648b79b0ef3dfcca844f23a53f8b5
edem4ik322 <rustemov.edem@mail.ru>
2025-06-18 15:58:45 +0300

    8.2

--- ad_project/frontend/src/views/Ads/AdListView.vue
+++ ad_project/frontend/src/views/Ads/AdListView.vue
@@ -1,7 +1,31 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <h1>Мои объявления</h1>
+      <v-col cols="12" sm="8" lg="6">
+        <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+        <v-card
+          v-for="ad in ads"
+          :key="ad.id"
+          class="mb-3"
+          max-width="1000"
+        >
+          <v-row>
+            <v-col xs="4">
+              <v-img :src="ad.src" height="175px" cover></v-img>
+            </v-col>
+            <v-col xs="8">
+              <h2 class="text--primary">{{ ad.title }}</h2>
+              <p style="height: 85px; overflow: hidden; text-overflow: ellipsis;">{{ ad.desc }}</p>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">
+                  Open
+                </v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -10,6 +34,36 @@
 export default {
   data () {
     return {
+      ads:[
+        {
+          title:"First",
+          desc:"First Desc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, urna eu tincidunt consectetur, nisi nisl aliquam nunc, eget aliquam massa nisl quis neque. Proin ac neque nec massa cursus cursus.",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id:"1"
+        },
+        {
+          title:"Second",
+          desc:"Second Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          id:"2"
+        },
+        {
+          title:"Third",
+          desc:"Thitd Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id:"3"
+        },
+        {
+          title:"Fouth",
+          desc:"Fouth Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id:"4"
+        }
+      ]
     }
   }
 }
